1a0d8a544f03e9137f9f1cc7f631749b8fd6953d
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-07-09 14:00:28 +1000

    autocomplete rendering

--- src/index.js
+++ src/index.js
@@ -61,6 +61,7 @@ ${movie.Title}
 option.addEventListener('click', () => {
     dropdown.classList.remove("is-active")
     input.value = movie.Title
+    onMovieSelect(movie);
 })
 ;
 resultsWrapper.appendChild(option)
@@ -78,3 +79,64 @@ resultsWrapper.appendChild(option)
         }
         
     })
+
+
+    const onMovieSelect = async movie => {
+        let id = movie.imdbID
+        const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        i: id
+      }
+      
+    }
+
+    );
+
+document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+    const movieTemplate = (movieDetail) => {
+return `
+<article class="media"> 
+<figure class="media-left">
+<p class="image">
+<img src="${movieDetail.Poster}" />
+
+
+</p>
+</figure>
+<div ="media-content">
+<div ="content">
+<h1>${movieDetail.Title} </h1>
+<h4>${movieDetail.Genre}</h4>
+<p>${movieDetail.Plot}</p>
+</div>
+</div>
+</article>
+<article="notification is-primary">
+<p class="title"> ${movieDetail.Awards} </p>
+<p class="subtitle"> Awards </p>
+</article>
+<article="notification is-primary"
+<p class="title"> ${movieDetail.BoxOffice} </p>
+<p class="subtitle"> Box Offices </p>
+</article>
+<article="notification is-primary"
+<p class="title"> ${movieDetail.Metascore} </p>
+<p class="subtitle"> Metascore</p>
+</article>
+<article="notification is-primary"
+<p class="title"> ${movieDetail.imdbRating} </p>
+<p class="subtitle"> IMDB Rating </p>
+</article>
+<article="notification is-primary"
+<p class="title"> ${movieDetail.Ratings[1].Value} </p>
+<p class="subtitle"> Rotten Tomatoes </p>
+</article>
+
+
+`
+
+
+}     
+    
